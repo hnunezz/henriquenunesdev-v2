74a789fedb73c87c3d4c0701302a2bdd7c0fe73c
hnunezz <nunesdealmeidahenrique@gmail.com>
2026-05-18 15:53:18 -0300

    yer on footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,22 +7,31 @@ import { TranslateModule } from '@ngx-translate/core';
   selector: 'app-footer',
   imports: [RouterLink, TranslateModule],
   template: `
-  <footer class="border-t border-solid border-gray-secondary py-12 flex justify-center items-center flex-col text-center gap-6 max-md:py-10">
-    <span class="hns-logo text-2xl text-ft-primary mb-2">hn.</span>
+    <footer
+      class="border-t border-solid border-gray-secondary py-12 flex justify-center items-center flex-col text-center gap-6 max-md:py-10"
+    >
+      <span class="hns-logo text-2xl text-ft-primary mb-2">hn.</span>
 
-    <nav class="flex gap-6 flex-wrap justify-center max-md:gap-4">
-      @for (nav of navigation(); track $index) {
-        <a class="text-sm font-medium cursor-pointer text-gray-primary transition-all duration-300 ease-in-out hover:text-primary-brand" [routerLink]="nav.anchor">{{nav.labelKey | translate}}</a>
-     }
-    </nav>
+      <nav class="flex gap-6 flex-wrap justify-center max-md:gap-4">
+        @for (nav of navigation(); track $index) {
+        <a
+          class="text-sm font-medium cursor-pointer text-gray-primary transition-all duration-300 ease-in-out hover:text-primary-brand"
+          [routerLink]="nav.anchor"
+          >{{ nav.labelKey | translate }}</a
+        >
+        }
+      </nav>
 
-    <span class="text-xs text-gray-primary mt-2">© 2025 Henrique Nunes ·
-     {{"FOOTER.DESCRIPTION" | translate}}</span>
-  </footer>
+      <span class="text-xs text-gray-primary mt-2"
+        >© {{ year }} Henrique Nunes ·
+        {{ 'FOOTER.DESCRIPTION' | translate }}</span
+      >
+    </footer>
   `,
 })
 export class FooterComponent {
   router = inject(Router);
   navigation = input<NavItem[]>([]);
 
+  year = new Date().getFullYear();
 }
